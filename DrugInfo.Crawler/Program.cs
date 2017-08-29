d845d3c8a2af1f78adeb2fe1730126beb99d1fa6
allen <378917466@qq.com>
2017-08-29 16:32:43 +0800

    药品监督检查

---
+++ DrugInfo.Crawler/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrugInfo.Crawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string optionType = "V1";
+            //查看第N页药品
+            string retrievedrugUrl = string.Format("http://app2.sfda.gov.cn/datasearchp/gzcxSearch.do?page={0}&searchcx=&optionType={1}&paramter0=null&paramter1=null&paramter2=null&formRender=cx", 1, optionType);
+
+
+
+        }
+    }
+}
