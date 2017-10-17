6e8f0315b6127d762549b419ef85a4c0d123a2ce
allen <378917466@qq.com>
2017-10-17 09:32:14 +0800

    随机数生成

---
+++ GenerateData/Form2.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace GenerateData
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelX6 = new DevComponents.DotNetBar.LabelX();
+            this.labelX4 = new DevComponents.DotNetBar.LabelX();
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            this.rangeSlider1 = new DevComponents.DotNetBar.Controls.RangeSlider();
+            this.textBoxX1 = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.labelX3 = new DevComponents.DotNetBar.LabelX();
+            this.dateTimeInput2 = new DevComponents.Editors.DateTimeAdv.DateTimeInput();
+            this.labelX2 = new DevComponents.DotNetBar.LabelX();
+            this.dateTimeInput1 = new DevComponents.Editors.DateTimeAdv.DateTimeInput();
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.textBoxX2 = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.labelX5 = new DevComponents.DotNetBar.LabelX();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.txtstarttemperature = new System.Windows.Forms.TextBox();
+            this.labelX7 = new DevComponents.DotNetBar.LabelX();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dateTimeInput2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateTimeInput1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelX6
+            // 
+            // 
+            // 
+            // 
+            this.labelX6.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX6.Location = new System.Drawing.Point(83, 69);
+            this.labelX6.Name = "labelX6";
+            this.labelX6.Size = new System.Drawing.Size(59, 23);
+            this.labelX6.TabIndex = 24;
+            // 
+            // labelX4
+            // 
+            // 
+            // 
+            // 
+            this.labelX4.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX4.Location = new System.Drawing.Point(29, 67);
+            this.labelX4.Name = "labelX4";
+            this.labelX4.Size = new System.Drawing.Size(75, 23);
+            this.labelX4.TabIndex = 22;
+            this.labelX4.Text = "温度范围";
+            // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX1.Location = new System.Drawing.Point(861, 72);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.Size = new System.Drawing.Size(75, 23);
+            this.buttonX1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX1.TabIndex = 21;
+            this.buttonX1.Text = "确认生成";
+            this.buttonX1.Click += new System.EventHandler(this.buttonX1_Click);
+            // 
+            // rangeSlider1
+            // 
+            // 
+            // 
+            // 
+            this.rangeSlider1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.rangeSlider1.FocusCuesEnabled = false;
+            this.rangeSlider1.Location = new System.Drawing.Point(148, 67);
+            this.rangeSlider1.Maximum = 100;
+            this.rangeSlider1.Name = "rangeSlider1";
+            this.rangeSlider1.RangeTooltipFormat = "Min: {0} - Max: {1}";
+            this.rangeSlider1.Size = new System.Drawing.Size(140, 24);
+            this.rangeSlider1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.rangeSlider1.TabIndex = 19;
+            this.rangeSlider1.Value = new DevComponents.DotNetBar.RangeValue(3, 6);
+            this.rangeSlider1.ValueChanged += new System.EventHandler(this.rangeSlider1_ValueChanged_1);
+            // 
+            // textBoxX1
+            // 
+            // 
+            // 
+            // 
+            this.textBoxX1.Border.Class = "TextBoxBorder";
+            this.textBoxX1.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.textBoxX1.Location = new System.Drawing.Point(701, 24);
+            this.textBoxX1.Name = "textBoxX1";
+            this.textBoxX1.PreventEnterBeep = true;
+            this.textBoxX1.Size = new System.Drawing.Size(100, 21);
+            this.textBoxX1.TabIndex = 18;
+            this.textBoxX1.Text = "1";
+            // 
+            // labelX3
+            // 
+            // 
+            // 
+            // 
+            this.labelX3.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX3.Location = new System.Drawing.Point(609, 24);
+            this.labelX3.Name = "labelX3";
+            this.labelX3.Size = new System.Drawing.Size(86, 23);
+            this.labelX3.TabIndex = 17;
+            this.labelX3.Text = "温度降低差值";
+            // 
+            // dateTimeInput2
+            // 
+            // 
+            // 
+            // 
+            this.dateTimeInput2.BackgroundStyle.Class = "DateTimeInputBackground";
+            this.dateTimeInput2.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.dateTimeInput2.ButtonDropDown.Shortcut = DevComponents.DotNetBar.eShortcut.AltDown;
+            this.dateTimeInput2.ButtonDropDown.Visible = true;
+            this.dateTimeInput2.CustomFormat = "yyyy-MM-dd HH:mm";
+            this.dateTimeInput2.Format = DevComponents.Editors.eDateTimePickerFormat.Custom;
+            this.dateTimeInput2.IsPopupCalendarOpen = false;
+            this.dateTimeInput2.Location = new System.Drawing.Point(377, 24);
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            this.dateTimeInput2.MonthCalendar.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.dateTimeInput2.MonthCalendar.CalendarDimensions = new System.Drawing.Size(1, 1);
+            this.dateTimeInput2.MonthCalendar.ClearButtonVisible = true;
+            // 
+            // 
+            // 
+            this.dateTimeInput2.MonthCalendar.CommandsBackgroundStyle.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.BarBackground2;
+            this.dateTimeInput2.MonthCalendar.CommandsBackgroundStyle.BackColorGradientAngle = 90;
+            this.dateTimeInput2.MonthCalendar.CommandsBackgroundStyle.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.BarBackground;
+            this.dateTimeInput2.MonthCalendar.CommandsBackgroundStyle.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.dateTimeInput2.MonthCalendar.CommandsBackgroundStyle.BorderTopColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.BarDockedBorder;
+            this.dateTimeInput2.MonthCalendar.CommandsBackgroundStyle.BorderTopWidth = 1;
+            this.dateTimeInput2.MonthCalendar.CommandsBackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.dateTimeInput2.MonthCalendar.DisplayMonth = new System.DateTime(2017, 9, 1, 0, 0, 0, 0);
+            // 
+            // 
+            // 
+            this.dateTimeInput2.MonthCalendar.NavigationBackgroundStyle.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
+            this.dateTimeInput2.MonthCalendar.NavigationBackgroundStyle.BackColorGradientAngle = 90;
+            this.dateTimeInput2.MonthCalendar.NavigationBackgroundStyle.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+            this.dateTimeInput2.MonthCalendar.NavigationBackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.dateTimeInput2.MonthCalendar.TodayButtonVisible = true;
+            this.dateTimeInput2.Name = "dateTimeInput2";
+            this.dateTimeInput2.Size = new System.Drawing.Size(200, 21);
+            this.dateTimeInput2.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.dateTimeInput2.TabIndex = 16;
+            // 
+            // labelX2
+            // 
+            // 
+            // 
+            // 
+            this.labelX2.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX2.Location = new System.Drawing.Point(313, 19);
+            this.labelX2.Name = "labelX2";
+            this.labelX2.Size = new System.Drawing.Size(58, 23);
+            this.labelX2.TabIndex = 15;
+            this.labelX2.Text = "结束时间";
+            // 
+            // dateTimeInput1
+            // 
+            // 
+            // 
+            // 
+            this.dateTimeInput1.BackgroundStyle.Class = "DateTimeInputBackground";
+            this.dateTimeInput1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.dateTimeInput1.ButtonDropDown.Shortcut = DevComponents.DotNetBar.eShortcut.AltDown;
+            this.dateTimeInput1.ButtonDropDown.Visible = true;
+            this.dateTimeInput1.CustomFormat = "yyyy-MM-dd HH:mm";
+            this.dateTimeInput1.Format = DevComponents.Editors.eDateTimePickerFormat.Custom;
+            this.dateTimeInput1.IsPopupCalendarOpen = false;
+            this.dateTimeInput1.Location = new System.Drawing.Point(97, 23);
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            this.dateTimeInput1.MonthCalendar.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.dateTimeInput1.MonthCalendar.CalendarDimensions = new System.Drawing.Size(1, 1);
+            this.dateTimeInput1.MonthCalendar.ClearButtonVisible = true;
+            // 
+            // 
+            // 
+            this.dateTimeInput1.MonthCalendar.CommandsBackgroundStyle.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.BarBackground2;
+            this.dateTimeInput1.MonthCalendar.CommandsBackgroundStyle.BackColorGradientAngle = 90;
+            this.dateTimeInput1.MonthCalendar.CommandsBackgroundStyle.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.BarBackground;
+            this.dateTimeInput1.MonthCalendar.CommandsBackgroundStyle.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.dateTimeInput1.MonthCalendar.CommandsBackgroundStyle.BorderTopColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.BarDockedBorder;
+            this.dateTimeInput1.MonthCalendar.CommandsBackgroundStyle.BorderTopWidth = 1;
+            this.dateTimeInput1.MonthCalendar.CommandsBackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.dateTimeInput1.MonthCalendar.DisplayMonth = new System.DateTime(2017, 9, 1, 0, 0, 0, 0);
+            // 
+            // 
+            // 
+            this.dateTimeInput1.MonthCalendar.NavigationBackgroundStyle.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
+            this.dateTimeInput1.MonthCalendar.NavigationBackgroundStyle.BackColorGradientAngle = 90;
+            this.dateTimeInput1.MonthCalendar.NavigationBackgroundStyle.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+            this.dateTimeInput1.MonthCalendar.NavigationBackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.dateTimeInput1.MonthCalendar.TodayButtonVisible = true;
+            this.dateTimeInput1.Name = "dateTimeInput1";
+            this.dateTimeInput1.Size = new System.Drawing.Size(200, 21);
+            this.dateTimeInput1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.dateTimeInput1.TabIndex = 14;
+            // 
+            // labelX1
+            // 
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Location = new System.Drawing.Point(37, 22);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(54, 23);
+            this.labelX1.TabIndex = 13;
+            this.labelX1.Text = "开始时间";
+            // 
+            // textBoxX2
+            // 
+            // 
+            // 
+            // 
+            this.textBoxX2.Border.Class = "TextBoxBorder";
+            this.textBoxX2.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.textBoxX2.Location = new System.Drawing.Point(469, 69);
+            this.textBoxX2.Name = "textBoxX2";
+            this.textBoxX2.PreventEnterBeep = true;
+            this.textBoxX2.Size = new System.Drawing.Size(100, 21);
+            this.textBoxX2.TabIndex = 26;
+            this.textBoxX2.Text = "1";
+            // 
+            // labelX5
+            // 
+            // 
+            // 
+            // 
+            this.labelX5.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX5.Location = new System.Drawing.Point(377, 69);
+            this.labelX5.Name = "labelX5";
+            this.labelX5.Size = new System.Drawing.Size(86, 23);
+            this.labelX5.TabIndex = 25;
+            this.labelX5.Text = "温度升高差值";
+            // 
+            // txtstarttemperature
+            // 
+            this.txtstarttemperature.Location = new System.Drawing.Point(701, 72);
+            this.txtstarttemperature.Name = "txtstarttemperature";
+            this.txtstarttemperature.Size = new System.Drawing.Size(100, 21);
+            this.txtstarttemperature.TabIndex = 27;
+            this.txtstarttemperature.Text = "5";
+            // 
+            // labelX7
+            // 
+            // 
+            // 
+            // 
+            this.labelX7.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX7.Location = new System.Drawing.Point(635, 70);
+            this.labelX7.Name = "labelX7";
+            this.labelX7.Size = new System.Drawing.Size(60, 23);
+            this.labelX7.TabIndex = 28;
+            this.labelX7.Text = "开始温度";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(861, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 29;
+            this.button1.Text = "导出";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(754, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "列数：0";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1112, 495);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelX7);
+            this.Controls.Add(this.txtstarttemperature);
+            this.Controls.Add(this.textBoxX2);
+            this.Controls.Add(this.labelX5);
+            this.Controls.Add(this.labelX6);
+            this.Controls.Add(this.labelX4);
+            this.Controls.Add(this.buttonX1);
+            this.Controls.Add(this.rangeSlider1);
+            this.Controls.Add(this.textBoxX1);
+            this.Controls.Add(this.labelX3);
+            this.Controls.Add(this.dateTimeInput2);
+            this.Controls.Add(this.labelX2);
+            this.Controls.Add(this.dateTimeInput1);
+            this.Controls.Add(this.labelX1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dateTimeInput2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateTimeInput1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.LabelX labelX6;
+        private DevComponents.DotNetBar.LabelX labelX4;
+        private DevComponents.DotNetBar.ButtonX buttonX1;
+        private DevComponents.DotNetBar.Controls.RangeSlider rangeSlider1;
+        private DevComponents.DotNetBar.Controls.TextBoxX textBoxX1;
+        private DevComponents.DotNetBar.LabelX labelX3;
+        private DevComponents.Editors.DateTimeAdv.DateTimeInput dateTimeInput2;
+        private DevComponents.DotNetBar.LabelX labelX2;
+        private DevComponents.Editors.DateTimeAdv.DateTimeInput dateTimeInput1;
+        private DevComponents.DotNetBar.LabelX labelX1;
+        private DevComponents.DotNetBar.Controls.TextBoxX textBoxX2;
+        private DevComponents.DotNetBar.LabelX labelX5;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox txtstarttemperature;
+        private DevComponents.DotNetBar.LabelX labelX7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ GenerateData/Form2.cs
@@ -0,0 +1,118 @@
+﻿using NPOIHelperTest;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenerateData
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            labelX6.Text = "(" + rangeSlider1.Value.Min + "-" + rangeSlider1.Value.Max + ")";
+            dateTimeInput1.Value = DateTime.Now.AddHours(-2);
+            dateTimeInput2.Value = DateTime.Now;
+
+        }
+        Dictionary<Guid, List<string>> dic = new Dictionary<Guid, List<string>>();
+        List<string> dateList = new List<string>();
+        bool generated = false;
+        decimal previousTemperature = 0;
+        private void buttonX1_Click(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>();
+
+            dic.Add(Guid.NewGuid(), list);
+            var random = new Random();
+            var temperature = decimal.Parse(txtstarttemperature.Text);
+            var min = rangeSlider1.Value.Min;
+            var max = rangeSlider1.Value.Max;
+            previousTemperature = temperature;
+            bool isUp = temperature < rangeSlider1.Value.Min;
+
+            list.Add(temperature.ToString("0.0"));
+            for (DateTime i = dateTimeInput1.Value; i < dateTimeInput2.Value; i = i.AddMinutes(1))
+            {
+                if (!generated)
+                {
+                    var date = i.ToString("yyyy年MM月dd日HH时mm分");
+                    dateList.Add(date);
+                }
+                if (isUp)
+                {
+                    var diff = int.Parse(textBoxX2.Text);
+                    var doublerandom = random.NextDouble();
+                    if (diff < 1)
+                    {
+                        diff = 0;
+                    }
+                    ///升高
+                    var diffvalue = random.Next(0, diff) + doublerandom;
+
+                    temperature += Convert.ToDecimal(diffvalue);
+                }
+                else
+                {
+                    var diff = int.Parse(textBoxX1.Text);
+                    var doublerandom = random.NextDouble();
+                    if (diff < 1)
+                    {
+                        diff = 0;
+                    }
+                    //下降
+                    var diffvalue = random.Next(0, int.Parse(textBoxX1.Text)) + doublerandom;
+                    temperature -= Convert.ToDecimal(diffvalue);
+                }
+                isUp = IsUp(temperature);
+                previousTemperature = temperature;
+                list.Add(temperature.ToString("0.0"));
+            }
+
+            label1.Text = "已有列数：" + dic.Count.ToString();
+            generated = true;
+        }
+
+        private bool IsUp(decimal temperature)
+        {
+            return temperature < rangeSlider1.Value.Min || (previousTemperature < temperature && temperature < rangeSlider1.Value.Max);
+        }
+
+        private void rangeSlider1_ValueChanged_1(object sender, EventArgs e)
+        {
+            labelX6.Invoke(new MethodInvoker(delegate { labelX6.Text = "(" + rangeSlider1.Value.Min + "-" + rangeSlider1.Value.Max + ")"; }));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataTable datatable = new DataTable();
+            datatable.Columns.Add(new DataColumn("日期"));
+
+            foreach (var item in dic.Keys)
+            {
+                datatable.Columns.Add(new DataColumn(item.ToString()));
+            }
+            for (int i = 0; i < dateList.Count; i++)
+            {
+                var newrow = datatable.NewRow();
+                newrow[0] = dateList[i];
+                foreach (var key in dic.Keys)
+                {
+                    newrow[key.ToString()] = dic[key][i];
+                }
+                datatable.Rows.Add(newrow);
+            }
+            saveFileDialog1.Filter = "(*.xls)|*.xls";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                NPOIHelper.DataTableToExcel(datatable, "温度数据", saveFileDialog1.FileName);
+            }
+        }
+    }
+}
